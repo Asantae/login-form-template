6bfd7c603486529f3e4f816e67568048cdec768e
Asantae Shelton <asantaems@gmail.com>
2022-06-11 02:35:34 -0400

    18min mark in tut video

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const env = require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+mongoose.connect(process.env.mongoBase, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
 
 const app = express()
 app.use('/', express.static(path.join(__dirname, 'static')))
@@ -8,9 +16,14 @@ app.use(bodyParser.json())
 
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
+    //Analysts
+    //Scripts reading databases
+
+    //Hashing the passwords
+     
     res.json({ status: 'ok'})
 })
 
-app.listen(5000, () => {
-    console.log('server up at 5000')
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
 })
